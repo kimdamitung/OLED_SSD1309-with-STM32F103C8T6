0f7fd76551a3115450bed4677b7dee37d84d0b9b
kimdamitung <150353472+kimdamitung@users.noreply.github.com>
2024-01-04 12:10:43 +0700

    Update ssd1309_conf_template.h

--- Debug/Core/Src/fonts.d
+++
@@ -1,1 +0,0 @@
-Core/Src/fonts.o: ../Core/Src/fonts.c
--- Debug/Core/Src/ssd1309.d
+++
@@ -1,4 +0,0 @@
-Core/Src/ssd1309.o: ../Core/Src/ssd1309.c ../Core/Inc/ssd1309.h \
- ../Core/Src/fonts.c
-../Core/Inc/ssd1309.h:
-../Core/Src/fonts.c:
